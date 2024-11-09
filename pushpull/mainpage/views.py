327453ecaa1e9739b50aedd430d3c9d9c92ef2a2
prestij1987 <lehaleyba@mail.ru>
2024-11-09 19:29:22 +0300

    Заработали отзывы

--- pushpull/mainpage/views.py
+++ pushpull/mainpage/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-#from django.http import HttpResponse
-
+from django.conf import settings
+from datetime import datetime
 
 
 def index(request):
@@ -11,6 +11,7 @@ def index(request):
                                 # podstanovka
         context
     )
+
 
 def uslugi(request):
     context = {}
@@ -30,7 +31,20 @@ def park(request):
         context
     )
 
+
 def otzyv(request):
+    if request.method == "POST":
+        otzyv_num = datetime.now().strftime('%Y%m%d%H%M%S.txt')
+        f = open(
+            settings.BASE_DIR / 'mainpage/templates/mainpage/otzyv/' / otzyv_num,
+            'w', encoding="utf-8")
+        f.write(request.POST['theme'] + '\n\n' + request.POST['txt'])
+        f.close()
+        return render(
+            request,
+            'mainpage/html/otzyvsaved.html',
+            context = {'otzyv_num': otzyv_num}
+            )
     context = {}
     return render(request,   
         'mainpage/html/otzyvi.html',  
