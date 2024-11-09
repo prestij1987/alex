1aa2c4900e7bca308e9db8f9d04c929defcc28ba
prestij1987 <lehaleyba@mail.ru>
2024-11-09 19:28:49 +0300

    Исправлены поля name

--- pushpull/mainpage/urls.py
+++ pushpull/mainpage/urls.py
@@ -4,10 +4,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',             views.index,  name='home'),
-    path('uslugi/',      views.uslugi, name='home'),
-    path('park/',        views.park, name='home'),
-    path('otzyvi/',      views.otzyv, name='home'),
+    path('',             views.index,      name='home'),
+    path('uslugi/',      views.uslugi,     name='uslugi'),
+    path('park/',        views.park,       name='park'),
+    path('otzyvi/',      views.otzyv,      name='otzyvi'),
     path('formaotvet/',  views.formaotvet, name='formaotvet'),
     #path('mystring/', func_start, name='mystring'),
     
